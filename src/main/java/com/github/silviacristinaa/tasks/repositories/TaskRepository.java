a6474722566f09f351888f908ffb087bc7245861
silviacristinaa <silviacristinaalexandre1@gmail.com>
2022-12-27 14:32:06 -0300

    [feature/05] - Criação endpoint findByFilters

--- src/main/java/com/github/silviacristinaa/tasks/repositories/TaskRepository.java
+++ src/main/java/com/github/silviacristinaa/tasks/repositories/TaskRepository.java
@@ -1,9 +1,33 @@
 package com.github.silviacristinaa.tasks.repositories;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.github.silviacristinaa.tasks.entities.Task;
+import com.github.silviacristinaa.tasks.enums.PriorityEnum;
+import com.github.silviacristinaa.tasks.enums.StatusEnum;
 
 public interface TaskRepository extends JpaRepository<Task, Long>{
-
+	
+	@Query("SELECT t FROM Task t WHERE (:keyword is null or t.title LIKE %:keyword% or t.description LIKE %:keyword%)"
+			+ "and ((:initialDateStartDate is null or :finalDateStartDate is null) or t.startDate BETWEEN "
+			+ ":initialDateStartDate AND :finalDateStartDate) "
+			+ "and ((:initialDateEndDate is null or :finalDateEndDate is null) or t.endDate BETWEEN "
+			+ ":initialDateEndDate AND :finalDateEndDate) "
+			+ "and (:priority is null or t.priority = :priority)"
+			+ "and (:status is null or t.status = :status)"
+			+ "and (:employeeId is null or t.employeeId = :employeeId)")
+	List<Task> findByKeywordAndStartDateBetweenAndEndDateBetweenAndPriorityAndStatusAndEmployeeId(
+			@Param("keyword") String keyword,
+			@Param("initialDateStartDate") LocalDate initialDateStartDate,
+			@Param("finalDateStartDate") LocalDate finalDateStartDate, 
+			@Param("initialDateEndDate") LocalDate initialDateEndDate,
+			@Param("finalDateEndDate") LocalDate finalDateEndDate, 
+			@Param("priority") PriorityEnum priority,
+			@Param("status") StatusEnum status,
+			@Param("employeeId") Long employeeId);
 }
--- src/main/java/com/github/silviacristinaa/tasks/services/TaskService.java
+++ src/main/java/com/github/silviacristinaa/tasks/services/TaskService.java
@@ -1,5 +1,7 @@
 package com.github.silviacristinaa.tasks.services;
 
+import java.time.LocalDate;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -7,6 +9,8 @@ import com.github.silviacristinaa.tasks.dtos.requests.TaskRequestDto;
 import com.github.silviacristinaa.tasks.dtos.requests.TaskStatusRequestDto;
 import com.github.silviacristinaa.tasks.dtos.responses.TaskResponseDto;
 import com.github.silviacristinaa.tasks.entities.Task;
+import com.github.silviacristinaa.tasks.enums.PriorityEnum;
+import com.github.silviacristinaa.tasks.enums.StatusEnum;
 import com.github.silviacristinaa.tasks.exceptions.BadRequestException;
 import com.github.silviacristinaa.tasks.exceptions.InternalServerErrorException;
 import com.github.silviacristinaa.tasks.exceptions.NotFoundException;
@@ -14,6 +18,10 @@ import com.github.silviacristinaa.tasks.exceptions.NotFoundException;
 public interface TaskService {
 	
 	Page<TaskResponseDto> findAll(Pageable pageable);
+	
+	Page<TaskResponseDto> findByFilters(String keyword, LocalDate initialDateStartDate, LocalDate finalDateStartDate, 
+			LocalDate initialDateEndDate, LocalDate finalDateEndDate, PriorityEnum priority, StatusEnum status, Long employeeId, 
+			Pageable pageable) throws BadRequestException;
 	
 	TaskResponseDto findOneTaskById(Long id) throws NotFoundException;
 	
